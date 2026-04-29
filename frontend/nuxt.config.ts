11439b755e58223dc3cf9ac04416da196987c83b
Rafi Ramadhan <bknrafiramadhan@email.com>
2026-04-30 03:35:57 +0700

    fix: perbaikan koneksi IPC

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,21 +1,17 @@
-import tailwindcss from "@tailwindcss/vite";
-
 export default defineNuxtConfig({
-  compatibilityDate: "2025-07-15",
+  ssr: true,
+  compatibilityDate: "2025-05-15",
   modules: ["@nuxt/icon", "@nuxt/fonts", "@nuxt/image", "@nuxt/ui"],
   devtools: { enabled: true },
   css: ["./app/assets/css/main.css"],
-  vite: {
-    plugins: [tailwindcss()],
-  },
   icon: {
     mode: "css",
     cssLayer: "base",
   },
   runtimeConfig: {
-      public: {
-         apiKey: '',
-         baseKey: '',
-      },
-   },
+    public: {
+      apiKey: "",
+      baseKey: "",
+    },
+  },
 });
