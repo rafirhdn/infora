7cee31bc32ec8075c6e185460e4f1c89bf0e3e04
Rafi Ramadhan <bknrafiramadhan@email.com>
2026-05-07 08:36:07 +0700

    fix: perbarui main.css dan nuxt.config.ts

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,40 +1,42 @@
 export default defineNuxtConfig({
-  ssr: true,
   compatibilityDate: "2025-05-15",
+
+  // Modules
   modules: ["@nuxt/fonts", "@nuxt/image", "@nuxt/ui"],
+
+  // Devtools
   devtools: { enabled: true },
+
+  // Css
   css: [
     "./app/assets/css/main.css",
-    '@fortawesome/fontawesome-svg-core/styles.css'
+    "@fortawesome/fontawesome-svg-core/styles.css",
   ],
-  
-  // Tambahkan konfigurasi untuk layout
-  components: [
-    {
-      path: '~/components',
-      pathPrefix: false,
-    },
-  ],
-  
-  // Pastikan layout terdeteksi
   features: {
     inlineStyles: true,
   },
-  
+
+  // Fonts
+  fonts: {
+    families: [
+      { name: "DM Sans", provider: "google" },
+      { name: "DM Mono", provider: "google" },
+    ],
+  },
+
+  // Components
+  components: [
+    {
+      path: "~/components",
+      pathPrefix: false,
+    },
+  ],
+
+  // Api
   runtimeConfig: {
     public: {
       apiKey: "",
       baseKey: "",
     },
   },
-  
-  // Konfigurasi nitro untuk development
-  nitro: {
-    devProxy: {
-      '/api': {
-        target: 'http://localhost:3001', // Sesuaikan dengan backend Anda
-        changeOrigin: true,
-      }
-    }
-  }
-})
+});
